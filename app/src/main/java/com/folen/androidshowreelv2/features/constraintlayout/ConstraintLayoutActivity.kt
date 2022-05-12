07380174e16afc1c51bee610e422861e3e30b092
mosz_kamil <kamil.mosz@apagroup.pl>
2022-05-12 11:05:51 +0200

    Updated libraries versions. Added library which shows image as popup. Added new color, added function that show inspiration image in popup.

--- app/src/main/java/com/folen/androidshowreelv2/features/constraintlayout/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/folen/androidshowreelv2/features/constraintlayout/ConstraintLayoutActivity.kt
@@ -1,12 +1,35 @@
 package com.folen.androidshowreelv2.features.constraintlayout
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.folen.androidshowreelv2.R
+import com.ceylonlabs.imageviewpopup.ImagePopup
+import com.folen.androidshowreelv2.databinding.ActivityConstraintLayoutBinding
+import com.folen.androidshowreelv2.utilities.Constants.CONSTRAINT_LAYOUT_INSPIRATION_IMAGE_URL
 
 class ConstraintLayoutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityConstraintLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_constraint_layout)
+        binding = ActivityConstraintLayoutBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.clickMe.setOnClickListener {
+            showLayoutInspirationImage()
+        }
+    }
+
+    private fun showLayoutInspirationImage() {
+        val imagePopup = ImagePopup(this)
+        imagePopup.windowHeight = 800
+        imagePopup.windowWidth = 800
+        imagePopup.isFullScreen = true
+        imagePopup.isHideCloseIcon = true
+        imagePopup.isImageOnClickClose = true
+        imagePopup.backgroundColor = Color.parseColor("#10FFFFFF")
+        imagePopup.initiatePopupWithGlide(CONSTRAINT_LAYOUT_INSPIRATION_IMAGE_URL)
+        imagePopup.viewPopup()
     }
 }
--- app/src/main/java/com/folen/androidshowreelv2/utilities/Constants.kt
+++ app/src/main/java/com/folen/androidshowreelv2/utilities/Constants.kt
@@ -2,4 +2,5 @@ package com.folen.androidshowreelv2.utilities
 
 object Constants {
     const val LIST_JSON_NAME = "list_data.json"
+    const val CONSTRAINT_LAYOUT_INSPIRATION_IMAGE_URL = "https://cdn.dribbble.com/users/4551562/screenshots/14259619/media/6cee1cfc5925da5e75eab620d3059cad.png"
 }
